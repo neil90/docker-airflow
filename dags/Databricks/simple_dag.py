7816909bee6c738c44fa667bb1fa4b3ca557116b
neil90 <neilpatel90@gmail.com>
2020-08-25 12:47:40 -0700

    Update with Databrick Dag and some additional configs

---
+++ dags/Databricks/simple_dag.py
@@ -0,0 +1,34 @@
+"""
+Code that goes along with the Airflow located at:
+http://airflow.readthedocs.org/en/latest/tutorial.html
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.databricks_operator import DatabricksRunNowOperator
+from datetime import datetime, timedelta
+
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2015, 6, 1),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'databricks_simple_dag',
+    default_args=default_args,
+    description='A simple Databricks DAG',
+    schedule_interval=None,
+    catchup=False
+)
+
+notebook_run = DatabricksRunNowOperator(task_id='sparkdbrtester', job_id=82, dag=dag)
